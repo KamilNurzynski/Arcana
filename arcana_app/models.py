3c91f37ee06584c5db6a544a5a59c53c828f7842
KamilNurzynski <nurzynski.kamil@gmail.com>
2022-11-09 14:41:35 +0100

    first part of models implemented. without migrations

--- arcana_app/models.py
+++ arcana_app/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from accounts.models import User
+
 
 # Create your models here.
+class Driver(models.Model):
+    id_card_type = models.CharField(max_length=128)
+    id_card_nr = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    birth_date = models.DateField()
+
+
+class Truck(models.Model):
+    country_of_registration = models.CharField(max_length=128)
+    registration_number = models.CharField(max_length=15)
+    year = models.CharField(max_length=4)
+    brand = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    vin_nr = models.CharField(max_length=17)
+    # MOT = zakres dat
+    # geolocation
+
+
+class Trailer(models.Model):
+    country_of_registration = models.CharField(max_length=128)
+    registration_number = models.CharField(max_length=15)
+    year = models.CharField(max_length=4)
+    brand = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    vin_nr = models.CharField(max_length=17)
+    # MOT = zakres dat
+
+
+class Insurance(models.Model):
+    pass
+
+
+class Freight(models.Model):
+    name = models.CharField(max_length=255)
+    nr_of_freight = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    cargo = models.CharField(max_length=255)
+    price = models.FloatField()
+    truck = models.ForeignKey(Truck)  # on_delete=models.CASCADE???
+    trailer = models.ForeignKey(Trailer)
+    insurance = models.ForeignKey(Insurance)
+    invoice = models.FileField()
+    loading_address_company_name = models.CharField(max_length=255)
+    loading_address_company_address_street = models.CharField(max_length=255)
+    loading_address_company_address_local_nr = models.CharField(max_length=255)
+    loading_address_company_address_postal_code = models.CharField(max_length=255)
+    loading_address_company_address_city = models.CharField(max_length=255)
+    loading_address_company_address_country = models.CharField(max_length=255)
+    unloading_address_company_name = models.CharField(max_length=255)
+    unloading_address_company_address_street = models.CharField(max_length=255)
+    unloading_address_company_address_local_nr = models.CharField(max_length=255)
+    unloading_address_company_address_postal_code = models.CharField(max_length=255)
+    unloading_address_company_address_city = models.CharField(max_length=255)
+    unloading_address_company_address_country = models.CharField(max_length=255)
+
+
+class Service(models.Model):
+    pass
+
+
+# spedycja ponizej
+class Forwarding(models.Model):
+    pass
